62d2998e9a49df35148e680dfae8d35e19b7e01d
Philipp Kreuzer <miezmoon64@gmail.com>
2021-12-19 10:07:53 +0100

    added citys

--- src/com/mieze/hexbattle/fields/building/City.java
+++ src/com/mieze/hexbattle/fields/building/City.java
@@ -1,6 +1,11 @@
 package com.mieze.hexbattle.fields.building;
 
 import java.awt.*;
+import java.awt.image.*;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
 
 import com.mieze.hexbattle.hex.Point;
 import com.mieze.hexbattle.HexPanel;
@@ -9,23 +14,58 @@ import com.mieze.hexbattle.fields.Field;
 
 public class City extends Building {
 	private static final double SIZE = 60;
-	private static Image lvl1, lvl2, lvl3, lvl4;
+	private static BufferedImage[] img = new BufferedImage[4];
 	private int lvl = 1;
-	
-    static {
-        Toolkit toolkit = Toolkit.getDefaultToolkit();
-        lvl1 = toolkit.getImage("assets/city_1.png");
-        scaleImage(lvl1, 32, 32);
-        lvl2 = toolkit.getImage("assets/city_2.png");
-        scaleImage(lvl2, 32, 32);
-        lvl3 = toolkit.getImage("assets/city_3.png");
-        scaleImage(lvl3, 32, 32);
-        lvl4 = toolkit.getImage("assets/city_4.png");
-        scaleImage(lvl4, 32, 32);
-    }
-	
+	private Color color = null;
+	private BufferedImage temp_img;
+
+	static {
+		try {
+			img[0] = ImageIO.read(new File("assets/city_1.png"));
+			scaleImage(img[0], 28, 28);
+			img[1] = ImageIO.read(new File("assets/city_2.png"));
+			scaleImage(img[1], 28, 28);
+			img[2] = ImageIO.read(new File("assets/city_3.png"));
+			scaleImage(img[2], 28, 28);
+			img[3] = ImageIO.read(new File("assets/city_4.png"));
+			scaleImage(img[3], 28, 28);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
 	public City(Field f) {
 		super(f);
+	}
+
+	private BufferedImage dye(BufferedImage image, Color color) {
+		if (!color.equals(this.color)) {
+			int w = image.getWidth();
+			int h = image.getHeight();
+
+			BufferedImage img = new BufferedImage(w, h, BufferedImage.TYPE_INT_ARGB);
+			Graphics2D g = img.createGraphics();
+
+			for (int x = 0; x < w; x++) {
+				for (int y = 0; y < h; y++) {
+					Color c = new Color(image.getRGB(x, y), true);
+					int alpha = c.getAlpha();
+					int gray = c.getRed();
+					g.setColor(
+							new Color(gray | color.getRed(), gray | color.getGreen(), gray | color.getBlue(), alpha));
+					g.fillRect(x, y, 1, 1);
+				}
+			}
+
+			g.dispose();
+
+			this.color = field.getOwner().getColor();
+
+			this.temp_img = img;
+			return img;
+		} else {
+			return temp_img;
+		}
 	}
 
 	@Override
@@ -37,6 +77,9 @@ public class City extends Building {
 	public void render(Graphics g, double zoom) {
 		Map map = field.getOwner().map;
 		Point pos = map.hexToDisplay(HexPanel.hexLayout.hexToPixel(field.getHex()));
-		g.drawImage(lvl1, (int)(pos.x - (SIZE*zoom)/2), (int)(pos.y - (SIZE*zoom)/2), (int)(SIZE*zoom), (int)(SIZE*zoom), null);
+		BufferedImage dyed = dye(img[lvl - 1], field.getOwner().getColor());
+
+		g.drawImage(dyed, (int) (pos.x - (SIZE * zoom) / 2), (int) (pos.y - (SIZE * zoom) / 2), (int) (SIZE * zoom),
+				(int) (SIZE * zoom), null);
 	}
 }
