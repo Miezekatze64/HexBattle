a58163d72f069a53f2283dd9835a63a7a58dcc39
Miezezkatze64 <miezemoon@gmail.com>
2022-01-05 14:22:16 +0100

    made tooltip class ```public```

--- src/com/mieze/hexbattle/toolbars/ToolbarButton.java
+++ src/com/mieze/hexbattle/toolbars/ToolbarButton.java
@@ -3,8 +3,6 @@ package com.mieze.hexbattle.toolbars;
 import com.mieze.hexbattle.Map;
 
 import java.awt.*;
-import java.util.List;
-import java.util.*;
 
 public abstract class ToolbarButton {
 	private boolean mouseOver = false;
@@ -19,7 +17,7 @@ public abstract class ToolbarButton {
 	public ToolbarButton(String name, Image img, String tooltipText) {
 		this.name = name;
 		this.image = img;
-		this.tooltip = new Tooltip(name, tooltipText);
+		this.tooltip = new Tooltip(name, tooltipText, true);
 	}
 
 	public ToolbarButton(String name, Image img) {
@@ -35,7 +33,7 @@ public abstract class ToolbarButton {
 		g.setColor(Color.RED);
 		
 		g.drawString(name, x-g.getFontMetrics(g.getFont()).stringWidth(name)/2, y+HEIGHT/2+6);
-
+		
 		if (mouseOver) tooltip.render(g, m, x, y);
 	}
 	
@@ -45,98 +43,6 @@ public abstract class ToolbarButton {
 
 	public boolean mouseOver() {
 		return mouseOver;
-	}
-
-	protected class Tooltip {
-		private String title, text;
-		private static final Font ftext = new Font("Helvetia", Font.PLAIN, 12);
-		private static final Font ftitle = new Font("Helvetia", Font.BOLD, 14);
-		private static final int w = 125;
-		List<String> strings;
-
-		public Tooltip(String title, String text) {
-			this.title = title;
-			this.text = text;
-		}
-
-		public void render(Graphics g, Map m, int x, int y) {
-			Font save = g.getFont();
-
-			g.setFont(ftext);
-			if (this.strings == null) this.strings = wrap(text, g.getFontMetrics(ftext), w-10);
-			final int h = (strings.size()+4)*15;
-
-			int rx = x-w/2;
-			int ry = y-h-45;			
-
-			g.setColor(Color.RED);
-			g.drawRect(rx, ry, w, h);
-
-			g.setColor(Color.BLACK);
-			for (int i = 0; i < strings.size(); i++) {
-				g.drawString(strings.get(i), rx+5, ry+(i+4)*15);
-			}
-
-			g.setFont(ftitle);
-			g.drawString(title, rx+7, ry+30);
-
-			g.setColor(Color.RED);
-
-			g.setFont(save);
-		}
-
-		private List<String> wrap(String txt, FontMetrics fm, int maxWidth){
-
-			List<String> list = new ArrayList<String>();
-			String line = "";
-			String lineBeforeAppend = "";
-
-			int pos = 0;
-
-			while (pos < txt.length()) {
-			
-				char next = txt.charAt(pos);
-				lineBeforeAppend = line;
-				line += next;
-
-				if (next == '\n') {
-					list.add(line.substring(0, line.length()-1));
-					line = "";
-					pos++;
-					continue;
-				}
-
-				int width = fm.stringWidth(line);
-
-
-				if (width  < maxWidth) {
-					pos++;
-					continue;
-				} else {
-					//new Line.
-					boolean space = false;
-					for (int i = line.length()-1; i >= 0; i--) {
-						if (line.charAt(i) == ' ') {
-							list.add(line.substring(0, i));
-							line = line.substring(i+1, line.length());
-							space = true;
-							break;
-						}
-					}
-					if (!space) {
-						list.add(lineBeforeAppend);
-						line = next+"";
-					}
-				}
-				pos++;
-			}
-
-			//the remaining part.
-			if(line.length() > 0){
-				list.add(line);
-			}
-			return list;
-		}
 	}
 
 }
